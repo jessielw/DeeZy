08ee4b5aba60e9077de0004c146eeea61f41308c
jlw4049 <jlw_4049@hotmail.com>
2023-03-28 17:31:33 -0400

    Add all functions outside of main here

--- packages/utils.py
+++ packages/utils.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import sys
+from argparse import ArgumentTypeError
+import subprocess
 
 
 def get_working_dir():
@@ -17,3 +19,53 @@ def get_working_dir():
     # we're running from a *.py file
     else:
         return Path.cwd()
+
+
+def validate_channels(value: any):
+    """Ensure we are utilizing the correct amount of channels.
+
+    Args:
+        value (any): Can be any input
+
+    Returns:
+        int: Input as an integer
+    """
+
+    valid_channels = [1, 2, 6]
+    if not value.isdigit():
+        raise ArgumentTypeError(f"Invalid input channels. Value must be an integer.")
+    value = int(value)
+    if value not in valid_channels:
+        raise ArgumentTypeError(
+            f"Invalid number of channels. Valid options: {valid_channels}"
+        )
+    return value
+
+
+def validate_track_index(value: any):
+    """
+    Determines if the input is a valid number.
+    If it's not returns the default of 0.
+
+    Args:
+        value (any): Can be any input
+
+    Returns:
+        int: Corrected track index
+    """
+
+    # Check if the input is valid
+    if value.isdigit():
+        return int(value)
+    # If the input is invalid, return the default value
+    return 0
+
+
+def process_job(cmd: list):
+    """Process jobs"""
+
+    with subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True
+    ) as proc:
+        for line in proc.stdout:
+            print(line.strip())
