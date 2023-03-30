0058c7be1f123b542226e12811fa1a6a73d95f03
xEsteem <76792853+xEsteem@users.noreply.github.com>
2023-03-30 22:47:32 +0100

    Adding support for ddp 7.1

--- packages/utils.py
+++ packages/utils.py
@@ -31,7 +31,7 @@ def validate_channels(value: any):
         int: Input as an integer
     """
 
-    valid_channels = [1, 2, 6]
+    valid_channels = [1, 2, 6, 8]
     if not value.isdigit():
         raise ArgumentTypeError(f"Invalid input channels. Value must be an integer.")
     value = int(value)
@@ -61,9 +61,34 @@ def validate_track_index(value: any):
     return 0
 
 
-def validate_bitrate(arg_parser: ArgumentParser, arguments: ArgumentParser.parse_args):
+def validate_channels_with_format(arg_parser: ArgumentParser, arguments: ArgumentParser.parse_args):
     """
-    Validate bitrate input based on channel input.
+    Validate channel count based on file format.
+    If an invalid input is detected, raise a parser error that will update
+    the user with valid options and exit the program automatically.
+
+    Args:
+        arg_parser (ArgumentParser): Parser instance
+        arguments (ArgumentParser.parse_args): Parsed arguments from parser instance
+    """
+    
+    
+    if arguments.format == "dd":
+        valid_channels = [1, 2, 6]
+    elif arguments.format == "ddp":
+        valid_channels = [1, 2, 6, 8]
+    else:
+        raise Exception("Unknown file format.")
+        
+    if arguments.channels not in valid_channels:
+            arg_parser.error(
+                message=f"Invalid channel count for designated file type: {arguments.format}.\nValid options: {valid_channels}"
+            )
+
+
+def validate_bitrate_with_channels_and_format(arg_parser: ArgumentParser, arguments: ArgumentParser.parse_args):
+    """
+    Validate bitrate input based on channel input and file format.
     If an invalid input is detected, raise a parser error that will update
     the user with valid options and exit the program automatically.
 
@@ -72,18 +97,30 @@ def validate_bitrate(arg_parser: ArgumentParser, arguments: ArgumentParser.parse
         arguments (ArgumentParser.parse_args): Parsed arguments from parser instance
     """
 
-    if arguments.channels == 1:
-        if arguments.bitrate not in allowed_bitrates.get("dd_10"):
+    if arguments.format == "dd":
+        if (arguments.channels == 1):
+            valid_bitrates = allowed_bitrates.get("dd_10")
+        if (arguments.channels == 2):
+            valid_bitrates = allowed_bitrates.get("dd_20")
+        if (arguments.channels == 6):
+            valid_bitrates = allowed_bitrates.get("dd_51")
+        else:
+            raise Exception("Invalid channel count.")
+    elif arguments.format == "ddp":
+        if (arguments.channels == 1):
+            valid_bitrates = allowed_bitrates.get("ddp_10")
+        if (arguments.channels == 2):
+            valid_bitrates = allowed_bitrates.get("ddp_20")
+        if (arguments.channels == 6):
+            valid_bitrates = allowed_bitrates.get("ddp_51")
+        if (arguments.channels == 8):
+            valid_bitrates = allowed_bitrates.get("ddp_71_standard")
+        else:
+            raise Exception("Invalid channel count.")
+    else:
+        raise Exception("Unknown file format.")
+        
+    if arguments.bitrate not in valid_bitrates:
             arg_parser.error(
-                message=f"Invalid bitrate for channel input of 1 (mono).\nValid choices: {', '.join(str(v) for v in allowed_bitrates.get('dd_10'))}"
-            )
-    elif arguments.channels == 2:
-        if arguments.bitrate not in allowed_bitrates.get("dd_20"):
-            arg_parser.error(
-                message=f"Invalid bitrate for channel input of 2 (stereo).\nValid choices: {', '.join(str(v) for v in allowed_bitrates.get('dd_20'))}"
-            )
-    elif arguments.channels == 6:
-        if arguments.bitrate not in allowed_bitrates.get("dd_51"):
-            arg_parser.error(
-                message=f"Invalid bitrate for channel input of 6 (5.1).\nValid choices: {', '.join(str(v) for v in allowed_bitrates.get('dd_51'))}"
+                message=f"Invalid bitrate for input channel count and file type: {arguments.format} {str(arguments.channels)}.\nValid options: {', '.join(str(v) for v in valid_bitrates)}"
             )
