30890fe274dc007cd7dcd7bf235c7f2aba25764e
jlw4049 <jlw_4049@hotmail.com>
2023-04-03 00:26:05 -0400

    - Organized dd/ddp function to generate XML and commented the code some - Changed Exception() to ArguementTypeError()
    - Created a new function to generate atmos xml
    - The template requires less information/less keyword args
    
    - Removed code that saved XML to directory from both functions, combined it in a private function at the top of the script for both of them to call
    - Additionally I added an error check to be raised in the event for some reason the file could not be written to disk

--- packages/xml.py
+++ packages/xml.py
@@ -2,6 +2,38 @@ import xmltodict
 from pathlib import Path
 from packages.xml_base import xml_audio_base_ddp, xml_audio_base_atmos
 from typing import Union
+from argparse import ArgumentTypeError
+
+
+def _save_xml(output_dir: Path, output_file_name: Path, xml_base: dict):
+    """Creates/Deletes old XML files for use with DEE
+
+    Args:
+        output_dir (Path): Full output directory
+        output_file_name (Path): File name
+        xml_base (dict): XML generated dictionary
+
+    Returns:
+        Path: Path to XML file for DEE
+    """
+    # Save out the updated template (use filename output with xml suffix)
+    updated_template_file = Path(output_dir / Path(output_file_name)).with_suffix(
+        ".xml"
+    )
+
+    # delete xml output template if one already exists
+    if updated_template_file.exists():
+        updated_template_file.unlink()
+
+    # write new xml template for dee
+    with open(updated_template_file, "w", encoding="utf-8") as xml_out:
+        xml_out.write(xmltodict.unparse(xml_base, pretty=True, indent="  "))
+
+    # check to ensure template file was created
+    if updated_template_file.exists():
+        return updated_template_file
+    else:
+        raise ArgumentTypeError("XML file could not be created")
 
 
 def generate_xml_dd(
@@ -14,7 +46,7 @@ def generate_xml_dd(
     output_file_name: str,
     output_dir: Union[Path, str],
 ):
-    """Handles the parsing/creation of XML file for dee
+    """Handles the parsing/creation of XML file for DEE encoding (DD/DDP)
 
     Args:
         down_mix_config (str): Down mix type ("off', 'mono', 'stereo', '5.1')
@@ -32,19 +64,13 @@ def generate_xml_dd(
     # Update the xml template values
     xml_base = xmltodict.parse(xml_audio_base_ddp)
 
-    # xml down mix config
-    xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"][
-        "downmix_config"
-    ] = down_mix_config
-    xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["data_rate"] = str(bitrate)
-
-    # xml wav config
+    # xml wav filename/path
     xml_base["job_config"]["input"]["audio"]["wav"]["file_name"] = f'"{wav_file_name}"'
     xml_base["job_config"]["input"]["audio"]["wav"]["storage"]["local"][
         "path"
     ] = f'"{str(output_dir)}"'
 
-    # xml ac3 config
+    # xml ac3 file/path
     xml_base["job_config"]["output"]["ac3"]["file_name"] = f'"{output_file_name}"'
     xml_base["job_config"]["output"]["ac3"]["storage"]["local"][
         "path"
@@ -53,7 +79,11 @@ def generate_xml_dd(
     # xml temp path config
     xml_base["job_config"]["misc"]["temp_dir"]["path"] = f'"{str(output_dir)}"'
 
-    # set down mix mode
+    # xml down mix config
+    xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"][
+        "downmix_config"
+    ] = down_mix_config
+
     if channels == 1:
         downmix_mode = "not_indicated"
     elif channels == 2:
@@ -64,14 +94,17 @@ def generate_xml_dd(
         "preferred_downmix_mode"
     ] = downmix_mode
 
+    # xml bit rate config
+    xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["data_rate"] = str(bitrate)
+
     # file format
     if dd_format == "dd":
         xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["encoder_mode"] = "dd"
     elif dd_format == "ddp":
 
-        # if ddp and normalize is true, adjust template to normalize audio
+        # if ddp and normalize is true, set template to normalize audio
         if normalize:
-            # Remove measure_only, add measure_and_correct, with default present of atsc_a85
+            # Remove measure_only, add measure_and_correct, with default preset of atsc_a85
             del xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["loudness"][
                 "measure_only"
             ]
@@ -82,32 +115,113 @@ def generate_xml_dd(
                 "measure_and_correct"
             ]["preset"] = "atsc_a85"
 
+        # if channels are 8 set encoder mode to ddp71
         if channels == 8:
             xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"][
                 "encoder_mode"
             ] = "ddp71"
+
+        # if channels are less than 8 set encoder to ddp
         else:
             xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"][
                 "encoder_mode"
             ] = "ddp"
+
+        # set output mode
         xml_base["job_config"]["output"]["ec3"] = xml_base["job_config"]["output"][
             "ac3"
         ]
+
+        # delete ac3 from dict
         del xml_base["job_config"]["output"]["ac3"]
     else:
-        raise Exception("Unknown file format.")
+        raise ArgumentTypeError("Unknown file format.")
 
-    # Save out the updated template (use ac3 output with xml suffix)
-    updated_template_file = Path(output_dir / Path(output_file_name)).with_suffix(
-        ".xml"
+    # create XML and return path to XML
+    updated_template_file = _save_xml(
+        output_dir=output_dir, output_file_name=output_file_name, xml_base=xml_base
     )
 
-    # delete xml output template if one already exists
-    if updated_template_file.exists():
-        updated_template_file.unlink()
+    return updated_template_file
 
-    # write new xml template for dee
-    with open(updated_template_file, "w", encoding="utf-8") as xml_out:
-        xml_out.write(xmltodict.unparse(xml_base, pretty=True, indent="  "))
+
+def generate_xml_atmos(
+    down_mix_config: str,
+    bitrate: str,
+    channels: int,
+    normalize: bool,
+    wav_file_name: str,
+    output_file_name: str,
+    output_dir: Union[Path, str],
+):
+    """Handles the parsing/creation of XML file for DEE encoding (atmos)
+
+    Args:
+        down_mix_config (str): Down mix type ("off', 'mono', 'stereo', '5.1')
+        bitrate (str): Bitrate in the format of '448'
+        channels: (int): Channels in the format of 1, 2 etc
+        normalize: (bool): True or False, if set to True we will normalize loudness
+        wav_file_name (str): File name only
+        output_file_name (str): File name only
+        output_dir (Union[Path, str]): File path only
+
+    Returns:
+        Path: Returns the correct path to the created template file
+    """
+    # Update the xml template values
+    xml_base = xmltodict.parse(xml_audio_base_atmos)
+
+    # xml wav filename/path
+    xml_base["job_config"]["input"]["audio"]["wav"]["file_name"] = f'"{wav_file_name}"'
+    xml_base["job_config"]["input"]["audio"]["wav"]["storage"]["local"][
+        "path"
+    ] = f'"{str(output_dir)}"'
+
+    # xml eac3 filename/path
+    xml_base["job_config"]["output"]["ec3"]["file_name"] = f'"{output_file_name}"'
+    xml_base["job_config"]["output"]["ec3"]["storage"]["local"][
+        "path"
+    ] = f'"{str(output_dir)}"'
+
+    # xml temp path config
+    xml_base["job_config"]["misc"]["temp_dir"]["path"] = f'"{str(output_dir)}"'
+
+    # xml down mix config
+    xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"][
+        "downmix_config"
+    ] = down_mix_config
+
+    if channels == 1:
+        downmix_mode = "not_indicated"
+    elif channels == 2:
+        downmix_mode = "ltrt"
+    elif channels >= 6:
+        downmix_mode = "loro"
+    xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["downmix"][
+        "preferred_downmix_mode"
+    ] = downmix_mode
+
+    # xml bitrate config
+    xml_base["job_config"]["filter"]["audio"]["encode_to_atmos_ddp"]["data_rate"] = str(
+        bitrate
+    )
+
+    # if normalize is true, set template to normalize audio
+    if normalize:
+        # Remove measure_only, add measure_and_correct, with default preset of atsc_a85
+        del xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["loudness"][
+            "measure_only"
+        ]
+        xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["loudness"][
+            "measure_and_correct"
+        ] = {}
+        xml_base["job_config"]["filter"]["audio"]["pcm_to_ddp"]["loudness"][
+            "measure_and_correct"
+        ]["preset"] = "atsc_a85"
+
+    # create XML and return path to XML
+    updated_template_file = _save_xml(
+        output_dir=output_dir, output_file_name=output_file_name, xml_base=xml_base
+    )
 
     return updated_template_file
