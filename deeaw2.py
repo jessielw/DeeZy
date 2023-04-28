57c54d02ec56a340aee252efa508ac0503735d20
jlw4049 <jlw_4049@hotmail.com>
2023-04-28 17:29:49 -0400

    - Organization

--- deeaw2.py
+++ deeaw2.py
@@ -1,6 +1,7 @@
-from audio_encoder import _main
+from cli import cli_parser
 from audio_encoder.utils.utils import _get_working_dir
+
 
 if __name__ == "__main__":
     base_wd = _get_working_dir()
-    _main(base_wd)
+    cli_parser(base_wd)
