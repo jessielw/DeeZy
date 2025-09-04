4dc0492c7b69cd85df030d072a13d3a26628d71a
jessielw <jessielw@gmail.com>
2025-09-04 19:08:52 -0400

    refactor: typing feat: slot optimization

--- deezy/utils/utils.py
+++ deezy/utils/utils.py
@@ -1,9 +1,11 @@
-import sys
 from pathlib import Path
+import sys
 
 
 class PrintSameLine:
     """Class to correctly print on same line"""
+
+    __slots__ = ("last_message",)
 
     def __init__(self):
         self.last_message = ""
@@ -14,7 +16,7 @@ class PrintSameLine:
         self.last_message = msg
 
 
-def _get_working_dir():
+def get_working_dir() -> Path:
     """
     Used to determine the correct working directory automatically.
     This way we can utilize files/relative paths easily.
