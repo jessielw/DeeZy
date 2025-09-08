e4cd4f54a8646ead38fa002f2b6ca51fbd61cf8f
jessielw <jessielw@gmail.com>
2025-09-07 23:56:25 -0400

    feat: add dd json base

---
+++ deezy/audio_encoders/dee/json/dd_base.py
@@ -0,0 +1,81 @@
+"""
+All 'None' values MUST be replaced during generation.
+"""
+
+dd_base = {
+    "job_config": {
+        "input": {
+            "audio": {
+                "wav": {
+                    "-version": "1",
+                    "file_name": None,  # str (full windows style double quoted path)
+                    "timecode_frame_rate": None,  # str
+                    "offset": "auto",
+                    "ffoa": "auto",
+                    "storage": {"local_multi_path": {"path": "-"}},
+                }
+            }
+        },
+        "filter": {
+            "audio": {
+                "pcm_to_ddp": {
+                    "-version": "3",
+                    "loudness": {
+                        "measure_only": {
+                            "metering_mode": None,  # str
+                            "dialogue_intelligence": None,  # bool
+                            "speech_threshold": None,  # int
+                        }
+                    },
+                    "encoder_mode": "dd",
+                    "bitstream_mode": "complete_main",
+                    "downmix_config": None,  # str
+                    "data_rate": None,  # int
+                    "timecode_frame_rate": None,  # str
+                    "start": "first_frame_of_action",
+                    "end": "end_of_file",
+                    "time_base": "embedded_timecode",
+                    "prepend_silence_duration": "0f",
+                    "append_silence_duration": "0f",
+                    "lfe_on": "true",
+                    "dolby_surround_mode": "not_indicated",
+                    "dolby_surround_ex_mode": "no",
+                    "user_data": "-1",
+                    "drc": {
+                        "line_mode_drc_profile": None,  # str
+                        "rf_mode_drc_profile": None,  # str
+                    },
+                    "lfe_lowpass_filter": None,  # bool
+                    "surround_90_degree_phase_shift": None,  # bool
+                    "surround_3db_attenuation": None,  # bool
+                    "downmix": {
+                        "loro_center_mix_level": None,  # str
+                        "loro_surround_mix_level": None,  # str
+                        "ltrt_center_mix_level": None,  # str
+                        "ltrt_surround_mix_level": None,  # str
+                        "preferred_downmix_mode": None,  # str
+                    },
+                    "allow_hybrid_downmix": "false",
+                    "embedded_timecodes": {
+                        "starting_timecode": "off",  # needs to always be off for compatibility and like ffmpeg
+                        "frame_rate": "auto",
+                    },
+                    "custom_dialnorm": None,  # str[int] (-1 - -31) but "0" disables it
+                }
+            }
+        },
+        "output": {
+            "ac3": {
+                "-version": "1",
+                "file_name": None,  # str (full windows style double quoted path)
+                "storage": {"local_multi_path": {"path": "-"}},
+            }
+        },
+        "misc": {
+            "temp_dir": {
+                "clean_temp": None,  # "true" or "false" string bool
+                "path": None,  # str
+            }
+        },
+    }
+}
