aeed2d8c91d633f4f321359dd208275322242081
jessielw <jessielw@gmail.com>
2025-09-08 13:27:23 -0400

    refactor: update

--- deezy/payloads/dd.py
+++ deezy/payloads/dd.py
@@ -1,11 +1,18 @@
 from dataclasses import dataclass
 
 from deezy.enums.dd import DolbyDigitalChannels
-from deezy.enums.shared import DeeDRC
+from deezy.enums.shared import StereoDownmix
 from deezy.payloads.shared import BaseArgsPayload
 
 
 @dataclass(slots=True)
 class DDPayload(BaseArgsPayload):
-    channels: DolbyDigitalChannels = DolbyDigitalChannels.AUTO
-    drc: DeeDRC = DeeDRC.FILM_LIGHT
+    channels: DolbyDigitalChannels
+    lfe_lowpass_filter: bool
+    surround_90_degree_phase_shift: bool
+    surround_3db_attenuation: bool
+    loro_center_mix_level: str
+    loro_surround_mix_level: str
+    ltrt_center_mix_level: str
+    ltrt_surround_mix_level: str
+    preferred_downmix_mode: StereoDownmix
