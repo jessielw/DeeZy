1eccdaeef4f515ea663d0f92ea59dc705313b926
jessielw <jessielw@gmail.com>
2025-09-08 13:23:02 -0400

    feat: re work dd module fix: numerous edge case bugs feat: now utilizes new payload feat: now utilize dee json work flow

--- deezy/audio_encoders/dee/dd.py
+++ deezy/audio_encoders/dee/dd.py
@@ -1,11 +1,9 @@
-from collections.abc import Sequence
 from pathlib import Path
 import shutil
 import tempfile
 
 from deezy.audio_encoders.dee.base import BaseDeeAudioEncoder
-from deezy.audio_encoders.dee.bitrates import dee_dd_bitrates
-from deezy.audio_encoders.dee.xml.xml import DeeXMLGenerator
+from deezy.audio_encoders.dee.json.dee_json_generator import DeeJSONGenerator
 from deezy.audio_encoders.delay import get_dee_delay
 from deezy.audio_processors.dee import process_dee_job
 from deezy.audio_processors.ffmpeg import process_ffmpeg_job
@@ -13,16 +11,20 @@ from deezy.enums.dd import DolbyDigitalChannels
 from deezy.enums.shared import StereoDownmix
 from deezy.exceptions import InvalidExtensionError, OutputFileNotFoundError
 from deezy.payloads.dd import DDPayload
+from deezy.payloads.shared import ChannelBitrates
 from deezy.track_info.mediainfo import MediainfoParser
+from deezy.utils.logger import logger
 
 
-class DDEncoderDEE(BaseDeeAudioEncoder):
+class DDEncoderDEE(BaseDeeAudioEncoder[DolbyDigitalChannels]):
+    """Dolby Digital Encoder."""
+
     def __init__(self, payload: DDPayload):
         super().__init__()
         self.payload = payload
 
     def encode(self):
-        # convert for dee XML
+        """Handles converting everything needed for DEE."""
         # file input
         file_input = Path(self.payload.file_input)
         self._check_input_file(file_input)
@@ -33,43 +35,55 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
         ).get_audio_track_info()
 
         # bitrate
-        bitrate = str(
-            self._get_closest_allowed_bitrate(
-                bitrate=self.payload.bitrate,
-                accepted_bitrates=self._get_accepted_bitrates(
-                    desired_channels=self.payload.channels,
-                    source_channels=int(audio_track_info.channels),
-                ),
-            )
+        # get object based off of desired channels and source audio track channels
+        bitrate_obj = self._get_channel_bitrate_object(
+            self.payload.channels, audio_track_info.channels
         )
+        # check to see if the users bitrate is allowed
+        runtime_bitrate = self.payload.bitrate
+        if runtime_bitrate and not bitrate_obj.is_valid_bitrate(runtime_bitrate):
+            fixed_bitrate = bitrate_obj.get_closest_bitrate(runtime_bitrate)
+            logger.info(
+                f"Bitrate {runtime_bitrate} is invalid for this configuration. "
+                f"Using the next highest allowed bitrate: {fixed_bitrate}."
+            )
+            runtime_bitrate = fixed_bitrate
+        else:
+            runtime_bitrate = bitrate_obj.default
+            logger.debug(f"No supplied bitrate, defaulting to {runtime_bitrate}.")
 
         # check for up-mixing if user has defined their own channel
-        if self.payload.channels != DolbyDigitalChannels.AUTO:
+        if self.payload.channels is not DolbyDigitalChannels.AUTO:
             self._check_for_up_mixing(
                 audio_track_info.channels, self.payload.channels.value
             )
 
         # else if user has not defined their own channel, let's find the highest channel count
         # based on their input
-        elif self.payload.channels == DolbyDigitalChannels.AUTO:
+        elif self.payload.channels is DolbyDigitalChannels.AUTO:
             audio_track_info.channels = self._determine_auto_channel_s(
                 audio_track_info.channels, DolbyDigitalChannels.get_values_list()
             )
 
             # update payload channels enum to automatic channel selection
             self.payload.channels = DolbyDigitalChannels(audio_track_info.channels)
+            logger.info(f"No supplied channels, defaulting to {self.payload.channels}.")
 
         # delay
         delay_str = "0ms"
         if self.payload.delay:
             delay_str = self.payload.delay
         delay = get_dee_delay(delay_str)
+        if delay:
+            logger.debug(f"Generated delay {delay.MODE}:{delay.DELAY}.")
 
         # fps
         fps = self._get_fps(audio_track_info.fps)
+        logger.debug(f"Detected FPS {fps.to_dee_cmd()}.")
 
         # temp dir
         temp_dir = self._get_temp_dir(file_input, self.payload.temp_dir)
+        logger.debug(f"Temp directory {temp_dir}.")
 
         # check disk space
         self._check_disk_space(
@@ -80,6 +94,7 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
 
         # temp filename
         temp_filename = Path(tempfile.NamedTemporaryFile(delete=False).name).name
+        logger.debug(f"Temp filename {temp_filename}.")
 
         # check to see if input channels are accepted by dee
         dee_allowed_input = self._dee_allowed_input(audio_track_info.channels)
@@ -91,17 +106,16 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
             self.payload.stereo_mix,
             dee_allowed_input,
         )
+        logger.debug(f"Downmix config {down_mix_config}.")
 
         # determine if FFMPEG downmix is needed
         ffmpeg_down_mix = False
         if (down_mix_config == "off" and not dee_allowed_input) or (
-            self.payload.channels.value == 2
-            and self.payload.stereo_mix == StereoDownmix.DPLII
+            self.payload.channels is DolbyDigitalChannels.STEREO
+            and self.payload.stereo_mix is StereoDownmix.DPLII
         ):
             ffmpeg_down_mix = self.payload.channels.value
-
-        # stereo mix
-        stereo_mix = str(self.payload.stereo_mix.name).lower()
+            logger.debug(f"FFMPEG downmix needed {ffmpeg_down_mix}.")
 
         # file output (if an output is a defined check users extension and use their output)
         if self.payload.file_output:
@@ -112,11 +126,15 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
                 )
         else:
             output = Path(audio_track_info.auto_name).with_suffix(".ac3")
+        logger.debug(f"Output path {output}.")
 
-        # Define .wav and .ac3/.ec3 file names (not full path)
-        # TODO can likely handle this better.
+        # define .wav and .ac3/.ec3 file names (not full path)
         wav_file_name = temp_filename + ".wav"
         output_file_name = temp_filename + ".ac3"
+        output_file_path = temp_dir / output_file_name
+        logger.debug(
+            f"File paths: {wav_file_name=}, {output_file_name=}, {output_file_path=}."
+        )
 
         # generate ffmpeg cmd
         ffmpeg_cmd = self._generate_ffmpeg_cmd(
@@ -130,52 +148,58 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
             output_dir=temp_dir,
             wav_file_name=wav_file_name,
         )
+        logger.debug(f"{ffmpeg_cmd=}.")
 
         # process ffmpeg command
         _ffmpeg_job = process_ffmpeg_job(
             cmd=ffmpeg_cmd,
-            progress_mode=self.payload.progress_mode,
             steps=True,
             duration=audio_track_info.duration,
             step_info={"current": 1, "total": 3, "name": "FFMPEG"},
+            no_progress_bars=self.payload.no_progress_bars,
         )
+        logger.debug(f"FFMPEG job: {_ffmpeg_job}.")
 
-        # generate XML
-        xml_generator = DeeXMLGenerator(
-            bitrate=bitrate,
-            input_file_name=wav_file_name,
-            output_file_name=output_file_name,
+        # generate JSON
+        json_generator = DeeJSONGenerator(
+            input_file_path=temp_dir / wav_file_name,
+            output_file_path=output_file_path,
             output_dir=temp_dir,
+        )
+        json_path = json_generator.dd_json(
+            payload=self.payload,
+            bitrate=runtime_bitrate,
             fps=fps,
             delay=delay,
-            drc=self.payload.drc,
-            atmos=False,
+            temp_dir=temp_dir,
         )
-        update_xml = xml_generator.generate_xml_dd(
-            down_mix_config=down_mix_config,
-            stereo_down_mix=stereo_mix,
-            channels=self.payload.channels,
-        )
+        logger.debug(f"{json_path=}.")
 
         # generate DEE command
-        dee_cmd = self._get_dee_cmd(
-            dee_path=Path(self.payload.dee_path), xml_path=update_xml
+        dee_cmd = self.get_dee_json_cmd(
+            dee_path=self.payload.dee_path, json_path=json_path
         )
+        logger.debug(f"{dee_cmd=}.")
 
         # process dee command
         step_info = {"current": 2, "total": 3, "name": "DEE measure"}
         _dee_job = process_dee_job(
-            cmd=dee_cmd, progress_mode=self.payload.progress_mode, step_info=step_info
+            cmd=dee_cmd,
+            step_info=step_info,
+            no_progress_bars=self.payload.no_progress_bars,
         )
+        logger.debug(f"Dee job: {_dee_job}.")
 
         # move file to output path
-        # TODO maybe print that we're moving the file, in the event it takes a min?
-        move_file = Path(shutil.move(Path(temp_dir / output_file_name), output))
-        # TODO maybe cheek if move_file exists and print success?
+        logger.info(f"Moving {output_file_path.name} to {output}.")
+        move_file = Path(shutil.move(output_file_path, output))
+        logger.info("Done.")
 
         # delete temp folder and all files if enabled
-        # TODO if set to no, maybe let the user know where they are stored maybe, idk?
-        self._clean_temp(temp_dir, self.payload.keep_temp)
+        if not self.payload.keep_temp:
+            logger.debug(f"Cleaning temp directory ({temp_dir}).")
+            self._clean_temp(temp_dir, self.payload.keep_temp)
+            logger.debug("Temp directory cleaned.")
 
         # return path
         if move_file.is_file():
@@ -184,29 +208,18 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
             raise OutputFileNotFoundError(f"{move_file.name} output not found")
 
     @staticmethod
-    def _get_accepted_bitrates(
+    def _get_channel_bitrate_object(
         desired_channels: DolbyDigitalChannels, source_channels: int
-    ) -> Sequence[int]:
+    ) -> ChannelBitrates:
         if desired_channels is DolbyDigitalChannels.AUTO:
             if source_channels == 1:
-                return list(dee_dd_bitrates.get("dd_10", ()))
-            elif source_channels == 2 or source_channels < 6:
-                dd_10 = dee_dd_bitrates.get("dd_10", ())
-                dd_20 = dee_dd_bitrates.get("dd_20", ())
-                return sorted(list(set(dd_10) & set(dd_20)))
-            elif source_channels >= 6:
-                dd_10 = dee_dd_bitrates.get("dd_10", ())
-                dd_20 = dee_dd_bitrates.get("dd_20", ())
-                dd_51 = dee_dd_bitrates.get("dd_51", ())
-                return sorted(list(set(dd_10) & set(dd_20) & set(dd_51)))
-        elif desired_channels is DolbyDigitalChannels.MONO:
-            return list(dee_dd_bitrates.get("dd_10", ()))
-        elif desired_channels is DolbyDigitalChannels.STEREO:
-            return list(dee_dd_bitrates.get("dd_20", ()))
-        elif desired_channels is DolbyDigitalChannels.SURROUND:
-            return list(dee_dd_bitrates.get("dd_51", ()))
-
-        raise ValueError("No channel layout found")
+                return DolbyDigitalChannels.MONO.get_bitrate_obj()
+            elif source_channels > 1 and source_channels < 6:
+                return DolbyDigitalChannels.STEREO.get_bitrate_obj()
+            else:
+                return DolbyDigitalChannels.SURROUND.get_bitrate_obj()
+        else:
+            return desired_channels.get_bitrate_obj()
 
     @staticmethod
     def _get_down_mix_config(
@@ -215,22 +228,25 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
         stereo_downmix: StereoDownmix,
         dee_allowed_input: bool,
     ) -> str:
+        # if channels and input channels are the same or
+        # channels are stereo and downmix is DPLII (fall back to using FFMPEG to mix the audio) or
+        # not in dee allowed input return AUTO ("off").
         if (
             (channels.value == input_channels)
             or (
-                channels == DolbyDigitalChannels.STEREO
-                and stereo_downmix == StereoDownmix.DPLII
+                channels is DolbyDigitalChannels.STEREO
+                and stereo_downmix is StereoDownmix.DPLII
             )
             or not dee_allowed_input
         ):
-            return "off"
-        elif channels == DolbyDigitalChannels.MONO:
-            return "mono"
-        elif channels == DolbyDigitalChannels.STEREO:
-            return "stereo"
-        elif channels == DolbyDigitalChannels.SURROUND:
-            return "5.1"
-        return "off"
+            return DolbyDigitalChannels.AUTO.to_dee_cmd()
+        elif channels in (
+            DolbyDigitalChannels.MONO,
+            DolbyDigitalChannels.STEREO,
+            DolbyDigitalChannels.SURROUND,
+        ):
+            return channels.to_dee_cmd()
+        return DolbyDigitalChannels.AUTO.to_dee_cmd()
 
     def _generate_ffmpeg_cmd(
         self,
@@ -255,8 +271,8 @@ class DDEncoderDEE(BaseDeeAudioEncoder):
         # resample and add dplii
         audio_filter_args = []
         if (
-            channels == DolbyDigitalChannels.STEREO
-            and stereo_down_mix == StereoDownmix.DPLII
+            channels is DolbyDigitalChannels.STEREO
+            and stereo_down_mix is StereoDownmix.DPLII
         ):
             if resample:
                 audio_filter_args = [
