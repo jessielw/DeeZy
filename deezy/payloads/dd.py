9c83012f1cbe9e4952dde5ac885d5d61fa12b972
jessielw <jessielw@gmail.com>
2025-09-15 20:07:11 -0400

    feat: organize payloads

--- deezy/payloads/dd.py
+++ deezy/payloads/dd.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 
 from deezy.enums.dd import DolbyDigitalChannels
-from deezy.payloads.shared import BaseArgsPayload
+from deezy.payloads.shared import StereoMixPayload
 
 
 @dataclass(slots=True)
-class DDPayload(BaseArgsPayload):
+class DDPayload(StereoMixPayload):
     channels: DolbyDigitalChannels
--- deezy/payloads/ddp.py
+++ deezy/payloads/ddp.py
@@ -2,9 +2,9 @@ from dataclasses import dataclass
 
 from deezy.enums.ddp import DolbyDigitalPlusChannels
 from deezy.enums.ddp_bluray import DolbyDigitalPlusBlurayChannels
-from deezy.payloads.shared import BaseArgsPayload
+from deezy.payloads.shared import StereoMixPayload
 
 
 @dataclass(slots=True)
-class DDPPayload(BaseArgsPayload):
+class DDPPayload(StereoMixPayload):
     channels: DolbyDigitalPlusChannels | DolbyDigitalPlusBlurayChannels
