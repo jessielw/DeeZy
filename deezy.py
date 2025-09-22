6ad9ed21677c529da61fef5493305ab19f3dafd8
jessielw <jessielw@gmail.com>
2025-09-21 22:43:03 -0400

    style

--- deezy.py
+++ deezy.py
@@ -1,6 +1,5 @@
 from deezy.__main__ import main
 
-
 if __name__ == "__main__":
     # Delegate to the package entrypoint which handles consistent
     # initialization (including stdout/stderr buffering) so both
